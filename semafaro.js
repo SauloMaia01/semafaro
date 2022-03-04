347203ac4cea91917407af543034ef3a780a931a
Saulo Rodrigues Maia <saulormaia@hotmail.com>
2022-03-04 14:05:16 -0300

    Projeto

---
+++ semafaro.js
@@ -0,0 +1,43 @@
+const img = document.getElementById('img')
+const buttons = document.getElementById('buttons')
+let colorIndex = 0
+let intervalId = null
+
+const trafficLight = (event) => {
+turnOn[event.target.id]()
+}
+
+/* 
+const nextIndex = () => colorIndex < 2 ? ++colorIndex : 0 
+
+    Uma outra forma de realizar o nextIndex é com o ternario, feito acima 
+*/
+
+const nextIndex = () => {
+    if (colorIndex < 2){
+        colorIndex++
+    }else {
+        colorIndex = 0
+    }
+}
+
+const changeColor = () => {
+    const colors = ['red', 'yellow','green']
+    const color = colors[colorIndex]
+    turnOn[color]()
+    nextIndex()
+}
+
+const stopAutomatic = () => {
+    clearInterval (intervalId)
+}
+
+const turnOn = {
+    'red': ()       => img.src = './img/vermelho.png',
+    'yellow': ()    => img.src = './img/amarelo.png',
+    'green': ()     => img.src = './img/verde.png',
+    'automatic':()  => intervalId = setInterval(changeColor, 1000)
+
+}
+
+buttons.addEventListener('click', trafficLight)
